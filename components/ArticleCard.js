169fd40b929fba37caa2de5e67f0e940c17e08ed
John C. Kealy <johnckealy.dev@gmail.com>
2022-09-10 10:39:05 +0100

    add luxon for dates

--- components/ArticleCard.js
+++ components/ArticleCard.js
@@ -1,7 +1,11 @@
 import { Image } from 'react-datocms'
 import Link from 'next/link'
+const { DateTime } = require("luxon");
 
 const ArticleCard = ({ articleImage, date, title, slug }) => {
+
+  const formattedDate = DateTime.fromISO(date).toLocaleString(DateTime.DATE_FULL)
+
   return (
     <>
       <Link href={slug}>
@@ -10,7 +14,7 @@ const ArticleCard = ({ articleImage, date, title, slug }) => {
             <Image data={articleImage} />
             <div className='p-2 md:p-4  min-h-[100px] min-w-[70%] h-full flex justify-between flex-col'>
               <h4>{title}</h4>
-              <div className='text-right text-gray-500 '>{date}</div>
+              <div className='text-right text-gray-500 '>{formattedDate}</div>
             </div>
           </div>
         </a>
